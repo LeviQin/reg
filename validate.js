0f0853521ceb547ec92e96eaacd1f304e80032a7
慕钦 <48786616+muqin6610@users.noreply.github.com>
2020-12-09 14:12:11 +0800

    重构

--- validate.js
+++ validate.js
@@ -1,83 +1,42 @@
 /**
-* 常用正则验证
+*@Name 常用正则验证
+*@Author  QinBiao(qinbiao_web@163.com)
+*@GitHub  https://github.com/muqin6610/reg
+*@Version 1.0
  */
 
-/**
- * 邮箱
- * @param {*} s
- */
-export function isEmail (s) {
-  return /^([a-zA-Z0-9._-])+@([a-zA-Z0-9_-])+((.[a-zA-Z0-9_-]{2,3}){1,2})$/.test(s)
-}
-
-/**
- * 手机号码
- * @param {*} s
- */
-export function isMobile (s) {
-  return /^1[0-9]{10}$/.test(s)
-}
-
-/**
- * 电话号码
- * @param {*} s
- */
-export function isPhone (s) {
-  return /^([0-9]{3,4}-)?[0-9]{7,8}$/.test(s)
-}
-
-/**
- * URL地址
- * @param {*} s
- */
-export function isURL (s) {
-  return /^http[s]?:\/\/.*/.test(s)
-}
-
-/**
-* 特殊字符
-*@param {*} s
- */
-export function isSpecial(s) {
-  return /[`~!@#$%^&*()_\-+=<>?:"{}|,.\/;'\\[\]·~！@#￥%……&*（）——\-+={}|《》？：“”【】、；‘’，。、]/im.test(s)
-}
-
-/**
-* 密码(只可以输入字母数字)
-*@param {*} s
- */
-export function isPassword(s) {
-  return /^[A-Za-z0-9]+$/.test(s)
-}
-
-/**
-* 身份证号（——15位一代身份证和18位二代身份证都包含在内
-*@param {*} s
- */
-export function isIDcard(s) {
-  return /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/.test(s)
-}
-
-/**
-* QQ号码
-*@param {*} s
- */
-export function isQQnum(s) {
-  return /^[1-9][0-9]{4,10}$/.test(s)
-}
-
-/**
-* 微信号码
-*@param {*} s
- */
-export function isWXnum(s) {
-  return /^[a-zA-Z]([-_a-zA-Z0-9]{5,19})+$/.test(s)
-}
-
-/**
-* 车牌号码
-*@param {*} s
- */
-export function isLicensePlate(s) {
-  return /^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-Z0-9]{4}[A-Z0-9挂学警港澳]{1}$/.test(s)
+class Reg {
+  constructor() {
+    this.regDatas = {
+      "负浮点数": /^-([1-9]\d*\.\d*|0\.\d*[1-9]\d*)$/,
+      "正浮点数": /^[1-9]\d*\.\d*|0\.\d*[1-9]\d*$/,
+      "整数": /^-?[1-9]\d*$/,
+      "正整数": /^[1-9]\d*$/,
+      "负整数": /^-[1-9]\d*$/,
+      "中国邮政编码": /[1-9]\d{5}(?!\d)/,
+      "日期格式": /^\d{4}-\d{1,2}-\d{1,2}/,
+      "汉字": /^[\u4e00-\u9fa5]{0,}$/,
+      "数字": /^[0-9]*$/,
+      "邮箱": /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/,
+      "手机": /^(0\d{2,3}\-)?([2-9]\d{6,7})+(\-\d{1,6})?$|^1(3|4|5|6|7|8|9)\d{9}$/g,
+      "电话": /^([0-9]{3,4}-)?[0-9]{7,8}$/,
+      "URL": /^http[s]?:\/\/.*/,
+      "特殊字符": /[`~!@#$%^&*()_\-+=<>?:"{}|,.\/;'\\[\]·~！@#￥%……&*（）——\-+={}|《》？：“”【】、；‘’，。、]/im,
+      "身份证": /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/,
+      "QQ": /^[1-9][0-9]{4,10}$/,
+      "微信": /^[a-zA-Z]([-_a-zA-Z0-9]{5,19})+$/,
+      "车牌": /^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-Z0-9]{4}[A-Z0-9挂学警港澳]{1}$/,
+    }
+  }
+  /**
+  * @param {*} value
+  * @param {string} str
+  * @return {boolean}
+  */
+  toRegular(value, str) {
+    if (!this.regDatas.hasOwnProperty(str)) {
+      throw new Error(str + "类型不存在,请输入正确的正则验证类型!")
+    }
+    return this.regDatas[str].test(value)
+  }
 }
